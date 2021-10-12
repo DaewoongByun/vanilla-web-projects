9a401919e9310628a3dbba5ee65e796d9417cbca
dwbyun16 <dwbyun16@gmail.com>
2021-10-12 21:44:14 +0900

    3. feat: add time bar and time bar click

--- 3-Custom-Video-Player/js/index.js
+++ 3-Custom-Video-Player/js/index.js
@@ -7,9 +7,11 @@ const store = {};
 function App() {
   init();
   $("video").addEventListener("ended", pauseVideo);
+  $("video").addEventListener("timeupdate", changeTime);
   $(".play-btn").addEventListener("click", playVideo);
   $(".pause-btn").addEventListener("click", pauseVideo);
   $(".stop-btn").addEventListener("click", stopVideo);
+  $(".bar-background").addEventListener("mousedown", handleBarClick);
 }
 function init() {
   $(".pause-btn").style.display = "none";
@@ -28,6 +30,24 @@ function pauseVideo() {
 function stopVideo() {
   pauseVideo();
   $("video").currentTime = 0;
+}
+function changeTime(e, clickedTime = -1) {
+  const video = $("video");
+  const duration = video.duration;
+  const curTime = clickedTime === -1 ? video.currentTime : clickedTime;
+  let percent = (curTime / duration) * 100;
+  percent = percent.toFixed(3);
+  $(".bar").style.width = `${percent}%`;
+  if (clickedTime !== -1) {
+    video.currentTime = clickedTime;
+  }
+}
+
+function handleBarClick(e) {
+  const width = e.currentTarget.offsetWidth;
+  const clickedX = e.offsetX;
+  const clickedTime = (clickedX / width) * $("video").duration;
+  changeTime(e, clickedTime);
 }
 
 App();
