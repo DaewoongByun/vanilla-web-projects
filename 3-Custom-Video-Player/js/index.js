46d4edd8ce4d2b007733b39f3456aa57fb92790b
dwbyun16 <dwbyun16@gmail.com>
2021-10-12 21:09:14 +0900

    3. feat: Add play/pause, stop

--- 3-Custom-Video-Player/js/index.js
+++ 3-Custom-Video-Player/js/index.js
@@ -4,6 +4,30 @@ const $ = (query) => document.querySelector(query);
 
 const store = {};
 
-function App() {}
+function App() {
+  init();
+  $("video").addEventListener("ended", pauseVideo);
+  $(".play-btn").addEventListener("click", playVideo);
+  $(".pause-btn").addEventListener("click", pauseVideo);
+  $(".stop-btn").addEventListener("click", stopVideo);
+}
+function init() {
+  $(".pause-btn").style.display = "none";
+}
+
+function playVideo() {
+  $("video").play();
+  $(".play-btn").style.display = "none";
+  $(".pause-btn").style.display = "";
+}
+function pauseVideo() {
+  $("video").pause();
+  $(".play-btn").style.display = "";
+  $(".pause-btn").style.display = "none";
+}
+function stopVideo() {
+  pauseVideo();
+  $("video").currentTime = 0;
+}
 
 App();
