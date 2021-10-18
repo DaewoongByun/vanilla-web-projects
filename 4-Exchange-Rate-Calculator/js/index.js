cfbd70aa2e9aa1c63a1ab829642a7898dc6a4094
dwbyun16 <dwbyun16@gmail.com>
2021-10-18 20:54:05 +0900

    4. feat: Add select change events

--- 4-Exchange-Rate-Calculator/js/index.js
+++ 4-Exchange-Rate-Calculator/js/index.js
@@ -8,10 +8,64 @@ const store = {
   fromAmount: 1,
   toAmount: 0,
   rate: 1000,
+  rates: {},
+  renderAmount: function () {
+    $(".from > input").value = this.fromAmount;
+    $(".to > input").value = this.toAmount;
+  },
+  renderRate: function () {
+    $(
+      ".text"
+    ).innerText = `1 ${this.fromCurrency} = ${this.rate} ${this.toCurrency}`;
+  },
+  setFromAcount: function (amount) {
+    this.fromAmount = amount;
+    this.toAmount = (this.fromAmount * this.rate).toFixed(2);
+    this.renderAmount();
+  },
+  setFromCurrency: async function (currency) {
+    this.fromCurrency = currency;
+    const response = await fetch(
+      `https://v6.exchangerate-api.com/v6/81a24801eabd8893c27453b3/latest/${this.fromCurrency}`
+    );
+    if (response.status !== 200) {
+      alert("데이터를 가져올수 없습니다.");
+      return;
+    }
+    const data = await response.json();
+    const rates = data.conversion_rates;
+    this.rates = rates;
+    this.rate = rates[this.toCurrency];
+    this.fromAmount = 1;
+    this.toAmount = (this.fromAmount * this.rate).toFixed(2);
+    this.renderRate();
+    this.renderAmount();
+  },
+  setToCurrency: function (currency) {
+    this.toCurrency = currency;
+    this.rate = this.rates[this.toCurrency];
+    this.toAmount = (this.fromAmount * this.rate).toFixed(2);
+    this.renderAmount();
+    this.renderRate();
+  },
 };
 
 function App() {
   init();
+  $(".from > input").addEventListener("input", function (e) {
+    const value = e.target.value;
+    if (value < 0 || !value) {
+      store.setFromAcount(0);
+    } else {
+      store.setFromAcount(parseInt(e.target.value));
+    }
+  });
+  $("#from-select").addEventListener("change", function (e) {
+    store.setFromCurrency(e.target.value);
+  });
+  $("#to-select").addEventListener("change", function (e) {
+    store.setToCurrency(e.target.value);
+  });
 }
 
 async function init() {
@@ -24,6 +78,7 @@ async function init() {
   }
   const data = await response.json();
   const rates = data.conversion_rates;
+  store.rates = rates;
   const currencyList = [];
   for (const currency in rates) {
     currencyList.push(currency);
