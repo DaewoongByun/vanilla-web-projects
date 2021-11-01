d856bf8525c6dae0eb660784f6afb8cf40e12b91
dwbyun16 <dwbyun16@gmail.com>
2021-11-01 23:16:33 +0900

    8. feat: 랜덤 검색 기능 구현

--- 8-Meal-Finder/js/index.js
+++ 8-Meal-Finder/js/index.js
@@ -4,12 +4,14 @@ const $ = (query) => document.querySelector(query);
 
 const store = {
   foods: [],
+  selectedFood: {},
 
   search: function (keyword) {
     if (!keyword.split(" ").join("")) {
       alert("검색어를 입력하세요!");
       return;
     }
+    this.selectedFood = {};
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${keyword}`)
       .then((res) => {
         if (res.status === 200) {
@@ -32,6 +34,23 @@ const store = {
         this.render();
       });
   },
+  randomSearch: function () {
+    fetch("https://www.themealdb.com/api/json/v1/1/random.php")
+      .then((res) => {
+        if (res.status === 200) {
+          return res.json();
+        } else {
+          alert("서버에러");
+        }
+      })
+      .then((data) => {
+        console.log(data);
+        this.selectedFood = data.meals[0];
+        this.foods = [];
+        $(".search-info").innerText = ``;
+        this.render();
+      });
+  },
   render: function () {
     const cards = this.foods
       .map(
@@ -42,6 +61,28 @@ const store = {
       )
       .join("");
     $(".search-result").innerHTML = cards;
+
+    if (this.selectedFood.idMeal) {
+      $(".selected-food").style.display = "flex";
+      $(".selected-food > h1").innerText = this.selectedFood.strMeal;
+      const img = `<img src=${this.selectedFood.strMealThumb} />`;
+      $(".selected-food > .image-container").innerHTML = img;
+      $(".selected-food .category").innerText = this.selectedFood.strCategory;
+      $(".selected-food .area").innerText = this.selectedFood.strArea;
+      $(".selected-food > .instructions").innerText =
+        this.selectedFood.strInstructions;
+
+      let tags = "";
+      for (let i = 1; i <= 20; i++) {
+        if (!this.selectedFood[`strIngredient${i}`]) break;
+        const ingredient = this.selectedFood[`strIngredient${i}`];
+        const measure = this.selectedFood[`strMeasure${i}`];
+        tags += `<div class='ingredient'>${ingredient} - ${measure}</div>`;
+      }
+      $(".selected-food > .ingredients").innerHTML = tags;
+    } else {
+      $(".selected-food").style.display = "none";
+    }
   },
 };
 
@@ -55,6 +96,9 @@ function App() {
   $(".icon-search").addEventListener("click", () => {
     const keyword = $(".search > input").value;
     store.search(keyword);
+  });
+  $(".icon-random").addEventListener("click", () => {
+    store.randomSearch();
   });
 }
 
